c841744d8d72daef92014038ade4f8d5a4d58d42
rodri <rodrigorodriguezmonge@gmail.com>
2024-02-16 09:00:24 +0100

    test: testContacto desarrollado

--- src/testing/TestContacto.java
+++ src/testing/TestContacto.java
@@ -1,7 +1,22 @@
 package testing;
 
+import modelo.javabean.Contacto;
+
 public class TestContacto {
      public static void main(String[] args){
+          Contacto contacto1 = new Contacto("Pedro", "Sanchez", "662301123","bizcochito69@gmail.com", "PSOE");
 
+          Contacto contacto2 = new Contacto();
+          contacto2.setNombre("Mariano");
+          contacto2.setApellidos("Rajoy");
+          contacto2.setTelefono("633103341");
+          contacto2.setEmail("tractor364@gmail.com");
+          contacto2.setEmpresa("PP");
+
+          Contacto [] contactos  = {contacto1,contacto2};
+
+          for (Contacto con : contactos){
+               System.out.println("Nombre: "+ con.getNombre() + " Apellido: "+con.getApellidos());
+          }
      }
 }
