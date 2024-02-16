7ec2ecedd52be70605e4cd852c5bc5d7b8cb33eb
rodri <rodrigorodriguezmonge@gmail.com>
2024-02-16 09:31:07 +0100

    feat: interface GestionAgendaDao metodos

---
+++ src/modelo/dao/GestionAgendaDao.java
@@ -0,0 +1,17 @@
+package modelo.dao;
+
+import modelo.javabean.Contacto;
+
+import java.util.ArrayList;
+
+public interface GestionAgendaDao {
+    ArrayList<Contacto> buscarTodos();
+    boolean altaContacto(Contacto contacto);
+    boolean eliminarContacto(Contacto contacto);
+    Contacto buscarUno(String nombre);
+    Contacto buscarTelefono(String telefono);
+    Contacto buscarEmail(String email);
+    ArrayList<Contacto> buscarPorTresPrimeros(String nombre);
+    boolean cambiarDatos(Contacto contacto);
+    ArrayList<Contacto> contactosPorEmpresa(String empresa);
+}
